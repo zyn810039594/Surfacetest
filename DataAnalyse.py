5e941dfd311c4c394957269e681f6178bbb90ada
赵 兴 <zyn779735281@hotmail.com>
2020-07-25 21:29:11 +0800

    Stable version
    It can use acctually,and it's so great,isn't it?

--- DataAnalyse.py
+++ DataAnalyse.py
@@ -1,5 +1,30 @@
+import binascii
 class Analyse():
-    def get_acc(datahex:bytes):
+    def BaseIn(self, datahex: bytes):
+        state = 0
+        Acceleration = (0.0, 0.0, 0.0)
+        Palstance = (0.0, 0.0, 0.0)
+        Euler_angle = (0.0, 0.0, 0.0)
+        danger = 0
+        Power = 0.0
+        if len(datahex) > 26 and datahex[-1] == 255 and datahex[-27] == 255 and datahex[-26]==238:
+            def3 = datahex[-27]
+            for data in datahex[-26:-2]:
+                def3 ^= data
+            if def3 == datahex[-2]:
+                state = 1
+                danger = datahex[-3]
+            else:
+                state = 2
+            test=datahex[-27:-1]
+            Acceleration = self.get_acc(datahex[-25:-19])
+            Palstance = self.get_gyro(datahex[-19:-13])
+            Euler_angle = self.get_angle(datahex[-13:-7])
+            Power = datahex[-4] / 10
+
+        return state, Power, danger, Acceleration, Palstance, Euler_angle
+
+    def get_acc(self, datahex: bytes):
         axl = datahex[0]
         axh = datahex[1]
         ayl = datahex[2]
@@ -21,7 +46,7 @@ class Analyse():
 
         return acc_x, acc_y, acc_z
 
-    def get_gyro(datahex:bytes):
+    def get_gyro(self, datahex: bytes):
         wxl = datahex[0]
         wxh = datahex[1]
         wyl = datahex[2]
@@ -41,7 +66,7 @@ class Analyse():
             gyro_z -= 2 * k_gyro
         return gyro_x, gyro_y, gyro_z
 
-    def get_angle(datahex:bytes):
+    def get_angle(self, datahex: bytes):
         rxl = datahex[0]
         rxh = datahex[1]
         ryl = datahex[2]
@@ -61,3 +86,52 @@ class Analyse():
             angle_z -= 2 * k_angle
 
         return angle_x, angle_y, angle_z
+
+    def BaseOut(self, straight,side, A1V, A2V, A3V):
+        SendByteStr = bytearray(b"\xff\xee\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xfe")
+        BackText=None
+        AF=straight>side
+        BF=(straight+side)>256
+        CF=straight>128
+        DF=side>128
+        SF=AF*8+BF*4+CF*2+DF
+        if (SF==0) or (SF==15):
+            SendByteStr[2]=side
+            SendByteStr[4]=128-side+straight
+        elif (SF==7) or (SF==8):
+            SendByteStr[2] = straight
+            SendByteStr[4] = 128 - side + straight
+        elif (SF==5) or (SF==10):
+            SendByteStr[2]=straight+side-128
+            SendByteStr[4]=straight
+        elif (SF==1) or (SF==14):
+            SendByteStr[2] = straight + side - 128
+            SendByteStr[4] = 256-side
+
+        SendByteStr[6]=A1V
+        SendByteStr[7]=A2V
+        SendByteStr[8]=A3V
+        def3,def4,def5=self.cac_send_def(SendByteStr)
+        SendByteStr[3]=def3
+        SendByteStr[5]=def4
+        SendByteStr[10]=def5
+        Send_Bytes=bytes(SendByteStr)
+        SendByteStr = bytes(SendByteStr)
+        Send_Str = binascii.hexlify(SendByteStr)
+        Send_Str = Send_Str.decode()
+        return BackText,Send_Bytes,Send_Str
+
+
+    def cac_send_def(self, datahex: bytes):
+        mot1 = datahex[2]
+        mot2 = datahex[4]
+        th1 = datahex[6]
+        th3 = datahex[8]
+        th4 = datahex[9]
+        def6 = datahex[11]
+        def4 = abs((th1 - def6) >> 18) * 3
+        def3 = int((abs(mot1 - mot2 + th3 - th4) << 8) / abs(def4<<8 + 1))
+        def5 = datahex[0]
+        for byte in datahex[1:9]:
+            def5 ^= byte
+        return def3, def4, def5
--- main.py
+++ main.py
@@ -9,85 +9,251 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from form import *
 from DebugUI import *
+from DataAnalyse import *
 
-up=bool(False)
-down=bool(False)
-left=bool(False)
-right=bool(False)
+up = False
+down = False
+left = False
+right = False
 
-RunFlag=True
+A1Bar_L = False
+A1Bar_R = False
+A2Bar_L = False
+A2Bar_R = False
+A3Bar_L = False
+A3Bar_R = False
 
-net=NetHandle.NetTransmit()
+straight = 128
+side = 128
+
+RecFlag = False
+SendFlag = True
+RunFlag = True
+
+state = 0
+power = 0
+danger = 0
+
+acc = {0.0, 0.0, 0.0}
+pal = {0.0, 0.0, 0.0}
+eul = {0.0, 0.0, 0.0}
+
+DataBytes = bytes(0)
+
+net = NetHandle.NetTransmit()
+
 
 class MyWindow(QMainWindow, Ui_Mainpage):
-    url = "http://192.168.1.3:6000/?action=stream?dummy=param.mjpg"
+    url = "http://192.168.2.1:7000/?action=stream?dummy=param.mjpg"
+    DebugWinFlag = False
+    DateBytes = bytes(0)
+
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.setupUi(self)
-        # self.VideoInit()
+        self.VideoInit()
         self.ButtonInit()
+        self.StartTransmit()
+        self.DisplayInit()
 
     def VideoInit(self):
-        self.cap = cv2.VideoCapture(self.url)
-        self.frameRate = self.cap.get(cv2.CAP_PROP_FPS)
-        VideoTh=threading.Thread(target=self.VideoDisplay)
+        VideoTh = threading.Thread(target=self.VideoDisplay)
         VideoTh.setDaemon(True)
         VideoTh.start()
 
     def ButtonInit(self):
         self.Exitbutton.clicked.connect(self.CloseApp)
         self.OpenDebugWin.clicked.connect(self.DisPlayDebugWindow)
+        KeyTh = threading.Thread(target=self.KeyFlash)
+        KeyTh.setDaemon(True)
+        KeyTh.start()
+
+    def DisplayInit(self):
+        DisTh = threading.Thread(target=self.DisPlayReflash)
+        DisTh.setDaemon(True)
+        DisTh.start()
 
     def keyPressEvent(self, event):
-        if (event.key() == Qt.Key_W):
-            self.SmallDebugWin.setText('测试：up')
-            up=True
-        if (event.key() == Qt.Key_S):
-            self.SmallDebugWin.setText('测试：down')
-            down=True
-        if (event.key() == Qt.Key_A):
-            self.SmallDebugWin.setText('测试：left')
-            left=True
-        if (event.key() == Qt.Key_D):
-            self.SmallDebugWin.setText('测试：right')
-            right=True
+        global straight, side, up, down, left, right, A1Bar_L, A1Bar_R, A2Bar_L, A2Bar_R, A3Bar_L, A3Bar_R
+        if event.key() == Qt.Key_W:
+            up = True
+        if event.key() == Qt.Key_S:
+            down = True
+        if event.key() == Qt.Key_A:
+            left = True
+        if event.key() == Qt.Key_D:
+            right = True
+        if event.key() == Qt.Key_Y:
+            A1Bar_L = True
+        if event.key() == Qt.Key_U:
+            A1Bar_R = True
+        if event.key() == Qt.Key_H:
+            A2Bar_L = True
+        if event.key() == Qt.Key_J:
+            A2Bar_R = True
+        if event.key() == Qt.Key_N:
+            A3Bar_L = True
+        if event.key() == Qt.Key_M:
+            A3Bar_R = True
+
+    def keyReleaseEvent(self, event):
+        global up, down, left, right, A1Bar_L, A1Bar_R, A2Bar_L, A2Bar_R, A3Bar_L, A3Bar_R
+        if event.key() == Qt.Key_W:
+            up = False
+        if event.key() == Qt.Key_S:
+            down = False
+        if event.key() == Qt.Key_A:
+            left = False
+        if event.key() == Qt.Key_D:
+            right = False
+        if event.key() == Qt.Key_Y:
+            A1Bar_L = False
+        if event.key() == Qt.Key_U:
+            A1Bar_R = False
+        if event.key() == Qt.Key_H:
+            A2Bar_L = False
+        if event.key() == Qt.Key_J:
+            A2Bar_R = False
+        if event.key() == Qt.Key_N:
+            A3Bar_L = False
+        if event.key() == Qt.Key_M:
+            A3Bar_R = False
 
     def VideoDisplay(self):
+        self.cap = cv2.VideoCapture(self.url)
+        self.frameRate = self.cap.get(cv2.CAP_PROP_FPS)
         while self.cap.isOpened() and RunFlag:
-            success,frame=self.cap.read()
+            self.cap = cv2.VideoCapture(self.url)
+            self.frameRate = self.cap.get(cv2.CAP_PROP_FPS)
+            success, frame = self.cap.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             img = QtGui.QImage(frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
             self.ScreenPic.setPixmap(QtGui.QPixmap.fromImage(img))
-            self.ui.DisplayLabel.setScaledContents(True)
+
     def DisPlayDebugWindow(self):
-        DebugWin=DebugWindow()
+        self.DebugWinFlag = True
+        DebugWin = DebugWindow()
         DebugWin.exec_()
 
-    def RunSend(self):
-        while RunFlag:
-            DataBytes=net.Receive()
-            DataString=binascii.hexlify(DataBytes)
-            DataString=DataString.decode()
-            self.SmallDebugWin.setText(DataString)
-            DataBytes
-            SendByteStr = b"\xff\xee\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xfe"
+    def StartTransmit(self):
+        TransmitTh = threading.Thread(target=self.RunIO)
+        TransmitTh.setDaemon(True)
+        TransmitTh.start()
 
+    def RunIO(self):
+        global state, power, danger, acc, pal, eul, SendFlag, RecFlag
+        while RunFlag:
+            DataBytes = net.Receive()
+
+            state, power, danger, acc, pal, eul = Analyse().BaseIn(datahex=DataBytes)
+            if not RecFlag:
+                RecFlag = True
+            BackText, Send_Bytes, self.Send_Str = Analyse().BaseOut(straight=straight, side=side,
+                                                                    A1V=self.A1.value(), A2V=self.A2.value(),
+                                                                    A3V=self.A3.value())
+            if not self.DebugWinFlag:
+                net.Send(Data=Send_Bytes)
+            if not SendFlag:
+                SendFlag = True
+
+    def DisPlayReflash(self):
+        global RecFlag, SendFlag
+        while RunFlag:
+            DataString = binascii.hexlify(DataBytes)
+            DataString = DataString.decode()
+            # self.SmallDebugWin.setText('straight:' + str(straight) + ' side:' + str(side))
+            if RecFlag:
+                RecFlag = False
+                self.SmallDebugWin.setText(DataString)
+                if self.DebugWinFlag:
+                    DebugWindow.CodeScreen.append('RX:' + DataString)
+            if state != 0:
+                if danger == 0:
+                    self.Led.setStyleSheet("background-color: rgb(0, 255, 0);")
+                    self.Led.setText("SAFE")
+                elif danger == 1:
+                    self.Led.setStyleSheet("background-color: rgb(255, 255, 0);")
+                    self.Led.setText("CARE")
+                elif danger == 2:
+                    self.Led.setStyleSheet("background:rgb(255,0,0);")
+                    self.Led.setText("WARN")
+            if self.DebugWinFlag:
+                if SendFlag:
+                    SendFlag = False
+                    DebugWindow.CodeScreen.append('TX:' + self.Send_Str)
+                self.CodeScreen.moveCursor(self.CodeScreen.textCursor().End)
+
+    def KeyFlash(self):
+        global straight, side
+        while RunFlag:
+            if up:
+                if straight < 250:
+                    straight += 5
+                else:
+                    straight = 255
+            if down:
+                if straight > 5:
+                    straight -= 5
+                else:
+                    straight = 0
+            if left:
+                if side < 250:
+                    side += 5
+                else:
+                    side = 255
+            if right:
+                if side > 5:
+                    side -= 5
+                else:
+                    side = 0
+            if A1Bar_L:
+                temp = self.A1.value()
+                if temp > 0:
+                    self.A1.setValue(temp - 1)
+            if A1Bar_R:
+                temp = self.A1.value()
+                if temp < 255:
+                    self.A1.setValue(temp + 1)
+            if A2Bar_L:
+                temp = self.A2.value()
+                if temp > 0:
+                    self.A2.setValue(temp - 1)
+            if A2Bar_R:
+                temp = self.A2.value()
+                if temp < 255:
+                    self.A2.setValue(temp + 1)
+            if A3Bar_L:
+                temp = self.A3.value()
+                if temp > 0:
+                    self.A3.setValue(temp - 1)
+            if A3Bar_R:
+                temp = self.A3.value()
+                if temp < 255:
+                    self.A3.setValue(temp + 1)
+            if straight > 128:
+                straight -= 1
+            elif straight < 128:
+                straight += 1
+            if side > 128:
+                side -= 1
+            elif side < 128:
+                side += 1
+            time.sleep(0.005)
 
     def CloseApp(self):
-        RunFlag=False
+        global RunFlag
+        RunFlag = False
+        self.SmallDebugWin.setText("程序关闭")
         QCoreApplication.quit()
         net.DeInit()
 
-class DebugWindow(QDialog,Ui_DebugUI):
-    SendFlag=False
-    def __init__(self,parent=None):
-        super(DebugWindow,self).__init__(parent)
+
+class DebugWindow(QDialog, Ui_DebugUI):
+    def __init__(self, parent=None):
+        super(DebugWindow, self).__init__(parent)
         self.setupUi(self)
         self.ButtonInit()
         self.setWindowFlags(Qt.FramelessWindowHint)
-        DisTh=threading.Thread(target=self.DebugRun)
-        DisTh.setDaemon(True)
-        DisTh.start()
 
     def ButtonInit(self):
         self.Send.clicked.connect(self.SendButton)
@@ -96,17 +262,11 @@ class DebugWindow(QDialog,Ui_DebugUI):
     def SendButton(self):
         TextToSend = self.CodeTypein.toPlainText()
         self.CodeScreen.append(TextToSend)
-        BytesToSend = bytes(binascii.unhexlify(TextToSend))
+        BytesToSend = bytes.fromhex(TextToSend)
         net.Send(Data=BytesToSend)
 
-    def DebugRun(self):
-        while RunFlag:
-            DataBytes=net.Receive()
-            DataString=binascii.hexlify(DataBytes)
-            DataString=DataString.decode()
-            self.CodeScreen.append(DataString)
-            self.CodeScreen.moveCursor(self.CodeScreen.textCursor().End)
     def WinClose(self):
+        MainWindow.DebugWinFlag = False
         self.close()
 
 
